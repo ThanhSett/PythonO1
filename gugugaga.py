ca34cd61c5756997deccf4335878d5ae9300781f
ThanhSett <122035071+ThanhSett@users.noreply.github.com>
2023-01-05 16:33:03 +0100

    gugugaaga

---
+++ gugugaga.py
@@ -0,0 +1,128 @@
+cislo= 3
+slovo=slovo
+migomigomigo=migomigomigo
+l
+l
+l
+l
+
+k
+yh
+tg
+fe
+fsefse
+few
+
+w
+
+d
+s
+ef
+fsefses
+eff
+efas
+df
+se
+fsefses
+fsefseef
+ed
+fsefseeff
+f
+fsefseef
+
+fsefseefsfs
+edfs
+fsfsefsedfserg
+efassefsedfs
+efassefsedfss
+fsefsefdf
+e
+sfdfes
+fsfsefsedfsergfe
+sfdfeses
+fsefsefdffe
+sfdfese
+sfdfeses
+sfdfesf
+edfsf
+df
+fe
+edfse
+sfdfesf
+edfsdfe
+sfdsfesdfeesdfe
+edfsg
+g
+g
+g
+g
+g
+g
+g
+g
+g
+g
+
+gg
+
+gggg
+
+ggggg
+ggg
+
+ggggdrfg
+
+we
+feeas
+f
+we
+fsfsefsedfsergfefgg
+r
+drsf
+efassefsedfsxfes
+fsefsefdffee
+fsefsefdfsf
+sfdfeserdf
+waefe
+sfdsfesdfeesdfes
+edfsdfedf
+efassefsedfsse
+efassefsedfsxfesf
+sfdsfesdfeesdfesf
+sfdfeserdfesf
+sfdfeserdfesfdsf
+efassefsedfsse
+fsefsefdfedsf
+segf
+sehdt
+ggggdrfgfgv
+zsdfe 
+ fesfesf 
+ se
+
+
+ hse
+ fhs
+ fhsse
+ fhssegsf
+ hsgfhefhehfeghfeg
+ hsgfhefhehfeghfeghf
+ geh
+ fg
+ hsgfhefhehfeghfeggf
+ hsgfhefhehfeghfegeg
+ fhssegsfhegfhe
+ ghf
+ gehfg
+hsgfhefhehfeghfegeggfh
+ghfhe
+f
+hsgfhefhehfeghfeghe
+ghf
+ghfhegfh
+edfsgfegh
+fhssegsfhegfheg
+hsgfhefhehfeghfeggeh
+f
+geh
+fhssegsfhegfhe
